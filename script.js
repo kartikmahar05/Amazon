fa7811f0c2656bf8f6f99ac5ec7c0fdc4df7186e
Kartik <kartikmahar5@gmail.com>
2024-07-27 11:22:25 +0530

    amazon

---
+++ script.js
@@ -0,0 +1,228 @@
+function border(){
+   let a=document.querySelector(".logo");
+   a.style.border="1px solid white";
+}
+function border1(){
+   let a=document.querySelector(".mark");
+   a.style.border="1px solid white";
+}
+function undo(){
+   let a=document.querySelector(".logo");
+   a.style.border="0px";
+}
+function undo1(){
+   let a=document.querySelector(".mark");
+   a.style.border="0px";
+}
+function border2(){
+   let a=document.querySelector(".hello");
+   a.style.border="1px solid white";
+}
+function undo2(){
+   let a=document.querySelector(".hello");
+   a.style.border="0px";
+
+}
+function border3(){
+   let a=document.querySelector(".return");
+   a.style.border="1px solid white";
+}
+
+function undo3(){
+   let a=document.querySelector(".return");
+   a.style.border="0px";
+}
+function border4(){
+   let a=document.querySelector(".all");
+   a.style.border="1px solid white";
+}
+function undo4(){
+   let a=document.querySelector(".all");
+   a.style.border="0px";
+}
+function border5(){
+   let a=document.querySelector(".today");
+   a.style.border="1px solid white";
+}
+function undo5(){
+   let a=document.querySelector(".today");
+   a.style.border="0px";
+}
+function border6(){
+   let a=document.querySelector(".Customer");
+   a.style.border="1px solid white";
+}
+function undo6(){
+   let a=document.querySelector(".Customer");
+   a.style.border="0px"}
+function border7(){
+      let a=document.querySelector(".Registry");
+      a.style.border="1px solid white";
+}
+function undo7(){
+      let a=document.querySelector(".Registry");
+      a.style.border="0px"}
+function border8(){
+         let a=document.querySelector(".gift");
+         a.style.border="1px solid white";
+      }
+function undo8(){
+         let a=document.querySelector(".gift");
+         a.style.border="0px"}
+function border9(){
+ let a=document.querySelector(".Sell");
+ a.style.border="1px solid white";
+         }
+function undo9(){
+ let a=document.querySelector(".Sell");
+ a.style.border="0px"}
+ function border10(){
+   let a=document.querySelector(".SHOP");
+   a.style.border="1px solid white";
+           }
+  function undo10(){
+   let a=document.querySelector(".SHOP");
+   a.style.border="0px";}
+function line(){
+   let b=document.querySelector(".gettoworKKK")
+   b.style.textDecoration="underline";
+}
+function Undoline(){
+   let b=document.querySelector(".gettoworKKK")
+   b.style.textDecoration="none";
+}
+function line1(){
+   let b=document.querySelector("#WWW")
+   b.style.textDecoration="underline";
+}
+function Undoline1(){
+   let b=document.querySelector("#WWW")
+   b.style.textDecoration="none";
+}
+function line2(){
+   let b=document.querySelector("#PPP")
+   b.style.textDecoration="underline";
+}
+function Undoline2(){
+   let b=document.querySelector("#PPP")
+   b.style.textDecoration="none";
+}
+function line3(){
+   let b=document.querySelector("#PP")
+   b.style.textDecoration="underline";
+}
+function Undoline3(){
+   let b=document.querySelector("#PP")
+   b.style.textDecoration="none";
+}
+function line4(){
+   let b=document.querySelector("#PO")
+   b.style.textDecoration="underline";
+}
+function Undoline4(){
+   let b=document.querySelector("#PO")
+   b.style.textDecoration="none";
+}
+function line5(){
+   let b=document.querySelector("#POPPE")
+   b.style.textDecoration="underline";
+}
+function Undoline5(){
+   let b=document.querySelector("#POPPE")
+   b.style.textDecoration="none";
+}
+function line6(){
+   let b=document.querySelector(".q")
+   b.style.textDecoration="underline";
+}
+function Undoline6(){
+   let b=document.querySelector(".q")
+   b.style.textDecoration="none";
+}
+function line7(){
+   let b=document.querySelector(".y")
+   b.style.textDecoration="underline";
+}
+function Undoline7(){
+   let b=document.querySelector(".y")
+   b.style.textDecoration="none";
+}
+function line8(){
+   let b=document.querySelector(".x")
+   b.style.textDecoration="underline";
+}
+function Undoline8(){
+   let b=document.querySelector(".x")
+   b.style.textDecoration="none";
+}
+function backg(){
+   let c=document.querySelector(".SlisT")
+   c.style.backgroundColor="#e0dcdc"
+}
+function backu(){
+   let c=document.querySelector(".SlisT")
+   c.style.backgroundColor="#f7fafa" 
+}
+function backg1(){
+   let c=document.querySelector(".TlisT")
+   c.style.backgroundColor="#e0dcdc"}
+function backu1(){
+   let c=document.querySelector(".TlisT")
+   c.style.backgroundColor="#f7fafa" 
+}
+function backg2(){
+   let c=document.querySelector(".DlisT")
+   c.style.backgroundColor="#e0dcdc"}
+function backu2(){
+   let c=document.querySelector(".DlisT")
+   c.style.backgroundColor="#f7fafa" 
+}
+function backg3(){
+   let c=document.querySelector(".GlisT")
+   c.style.backgroundColor="#e0dcdc"}
+function backu3(){
+   let c=document.querySelector(".GlisT")
+   c.style.backgroundColor="#f7fafa" 
+}
+function backg4(){
+   let c=document.querySelector(".HlisT")
+   c.style.backgroundColor="#e0dcdc"}
+function backu4(){
+   let c=document.querySelector(".HlisT")
+   c.style.backgroundColor="#f7fafa" 
+}
+function backg5(){
+   let c=document.querySelector(".JlisT")
+   c.style.backgroundColor="#e0dcdc"}
+function backu5(){
+   let c=document.querySelector(".JlisT")
+   c.style.backgroundColor="#f7fafa" 
+}
+function backg6(){
+   let c=document.querySelector(".KlisT")
+   c.style.backgroundColor="#e0dcdc"}
+function backu6(){
+   let c=document.querySelector(".KlisT")
+   c.style.backgroundColor="#f7fafa" 
+}
+function backg7(){
+   let c=document.querySelector(".LlisT")
+   c.style.backgroundColor="#e0dcdc"}
+function backu7(){
+   let c=document.querySelector(".LlisT")
+   c.style.backgroundColor="#f7fafa" 
+}
+function backg8(){
+   let c=document.querySelector(".ZlisT")
+   c.style.backgroundColor="#e0dcdc"}
+function backu8(){
+   let c=document.querySelector(".ZlisT")
+   c.style.backgroundColor="#f7fafa" 
+}
+function backg9(){
+   let c=document.querySelector(".XlisT")
+   c.style.backgroundColor="#e0dcdc"}
+function backu9(){
+   let c=document.querySelector(".XlisT")
+   c.style.backgroundColor="#f7fafa" 
+}
